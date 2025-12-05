8dce48673ac2fb103664e826525aac286f82b5ef
Alena <96665824+AlexTDE@users.noreply.github.com>
2025-12-05 20:07:19 +0300

    Fix main urls.py to use custom admin_site and restore web interface

--- autotest_ui/autotest_ui/urls.py
+++ autotest_ui/autotest_ui/urls.py
@@ -1,37 +1,25 @@
 """
 URL configuration for autotest_ui project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path
 
+# Импортируем кастомный admin site
+from testsystem.admin import admin_site
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Админ-панель (кастомная с аналитикой)
+    path('admin/', admin_site.urls),
+    
+    # API endpoints
     path('api/', include('testsystem.urls')),
+    
+    # Веб-интерфейс (главная страница, тест-кейсы, прогоны)
     path('', include('testsystem.urls_web')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
-# Редирект на страницу входа для неавторизованных пользователей
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.cache import never_cache
-from django.contrib.auth.views import redirect_to_login
-
-# Если пользователь не авторизован и пытается зайти на главную, редиректим на login
-from django.conf import settings
+# Media files в development режиме
 if settings.DEBUG:
-    LOGIN_URL = '/login/'
-    LOGIN_REDIRECT_URL = '/'
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
